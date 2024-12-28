67a60efaadc3f235416f54a97484bc694e8a0cce
shayanzad <shayanazadbakht1781@gmail.com>
2024-12-28 08:56:55 +0330

    f

--- components/main/header_user.vue
+++ components/main/header_user.vue
@@ -1,3 +1,5 @@
+<!-- @format -->
+
 <template>
   <div class="iii p-1 flex justify-content-center">
     <div class="flex max-w-[1280px] justify-start w-[100%] m-auto">
@@ -5,11 +7,22 @@
         <div class="flex items-center lg:!hidden" @click="toggleMenu()">
           <v-icon class="mx-2 cursor-pointer" title="">mdi-menu</v-icon>
         </div>
-        <img class="w-[150px]" src="../../assets/images/bargram.png" />
+        <a href="https://bargram.ir">
+          <img class="w-[150px]" src="../../assets/images/bargram.png" />
+        </a>
       </div>
       <div class="flex justify-between items-center w-full">
-        
-        
+        <div class="mx-10">
+          <a href="https://bargram.ir">
+            <span class=" hover:bg-slate-50 p-2"> صفحه اصلی </span>
+          </a>
+          <span
+            class="mx-3 rounded-md p-2  "
+            style="border: 2px solid orange;color: orange;"
+          >
+              محاسبه کرایه
+          </span>
+        </div>
       </div>
     </div>
   </div>
